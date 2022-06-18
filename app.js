8bbead7ace992299238013846d965cb874c0a812
jiwon kim <jiwon@jiwonui-MacBookAir.local>
2022-06-18 09:04:14 +0900

    color-stroke

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 500;
 canvas.height = 500;
@@ -30,9 +31,18 @@ function stopPainting(event) {
   painting = false;
 }
 
+function handleColor(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColor)
+);
